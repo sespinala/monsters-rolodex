a6f8f877c0738f3eeaee8808ae9b7fc1d7840d0c
Sara Espinal <saraespinal1@gmail.com>
2019-11-28 14:50:16 -0500

    Updates

---
+++ precache-manifest.bf70b366b31a2148318d3c70d1129b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ec48e2529df27ea1c2d170e841eb1d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a89868013749ddc4054",
+    "url": "/monsters-rolodex/static/css/main.4c699c67.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "0a89868013749ddc4054",
+    "url": "/monsters-rolodex/static/js/main.9ad74d9c.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
